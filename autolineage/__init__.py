47eeaca125d03dddb486ef10f3b9088819d23eb8
kishanraj41 <41926128+kishanraj41@users.noreply.github.com>
2026-02-15 21:20:38 -0600

    Enhance __init__.py with versioning and Jupyter support
    Added versioning, author information, and Jupyter extension support.

--- autolineage/__init__.py
+++ autolineage/__init__.py
@@ -1,12 +1,49 @@
-# Auto Lineage
-
 """
-This module provides tracing of changes and transformations in the data lineage.
+AutoLineage - Automatic ML Data Lineage Tracking
+
+Track your data lineage automatically from raw data to trained models
+without manual logging.
 """
 
-def get_lineage(data):
-    """Function to retrieve lineage of the input data."""
-    # Implementation goes here
-    pass
+__version__ = "0.1.0"
+__author__ = "Kishan Raj"
+__email__ = "kishanraj41@gmail.com"
 
-# Additional functionality can be added here
+# Import main classes for easy access
+from .database import LineageDatabase
+from .tracker import DatasetTracker, hash_file, get_file_info
+from .graph import LineageGraph
+from .reporter import ComplianceReporter
+
+# Define what's available when doing "from autolineage import *"
+__all__ = [
+    'LineageDatabase',
+    'DatasetTracker',
+    'LineageGraph',
+    'ComplianceReporter',
+    'hash_file',
+    'get_file_info',
+    '__version__',
+]
+
+
+def get_version():
+    """Return the current version."""
+    return __version__
+
+
+# Package-level configuration
+DEFAULT_DB_PATH = "lineage.db"
+
+
+# Jupyter extension
+def load_ipython_extension(ipython):
+    """Load Jupyter magic commands."""
+    from .magic import load_ipython_extension as load_magic
+    load_magic(ipython)
+
+
+def unload_ipython_extension(ipython):
+    """Unload Jupyter magic commands."""
+    from .magic import unload_ipython_extension as unload_magic
+    unload_magic(ipython)
